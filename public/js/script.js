94be82b7b369aa6190f1165be063c871d0283ca6
Fauzan2617 <mfauzandwiputera10@gmail.com>
2024-11-27 13:04:11 +0700

    hal-utama-part1

---
+++ public/js/script.js
@@ -0,0 +1,5 @@
+// Ini adalah file JavaScript Anda. Gunakan untuk menambahkan interaktivitas pada halaman Anda jika diperlukan.
+
+// Contoh kode JavaScript.
+
+console.log('Selamat datang di website makanan!');
